fbe98ba2cee910b23775a085ed08ebce910b8ccf
Bharat Lodhi <43291811+BharatLodhi@users.noreply.github.com>
2019-02-04 17:29:42 +0530

    Update flask.js

--- flask.js
+++ flask.js
@@ -1,45 +1,11 @@
-/*var parameters = {
-  target: '#Sum',
-  data: [{
-    fn: 'sin(x)', 
-    color: 'red'
- }],
-  grid: true,
-  yAxis: {domain: [-1, 1]},
-  xAxis: {domain: [0, 2*Math.PI]}
-};
-
-function plot() {
-  var f = document.querySelector("#function").value;
-  var xMin = document.querySelector("#xMin").value;
-  var xMax = document.querySelector("#xMax").value;
-  var yMin = document.querySelector("#yMin").value;
-  var yMax = document.querySelector("#yMax").value;
-  var color = document.querySelector("#color").value;
-  
-  parameters.data[0].fn = f;
-  parameters.xAxis.domain = [xMin, xMax];
-  parameters.yAxis.domain = [yMin, yMax];
-  parameters.data[0].color = color;
-  
-  functionPlot(parameters);
-}
-
-*/
-// function summer(x){
-// return(x);    
-// }
-
-var n = 50; // number of points
-var max = 100; // maximum of x and y
 
 // dimensions and margins
 var width=500;
 var height=500;
 
 var svg = d3.select("#Sum1")
-	.append("svg")
-	.attr("width", width)
+    .append("svg")
+    .attr("width", width)
     .attr("height",height);
 
 width=0.8*width;
@@ -89,7 +55,7 @@ var txt = points_g.selectAll("text").data(data);
 points = points.enter().append("circle")
       .attr('cx', function(d) {return xScale(d[0]);})
       .attr('cy', function(d) {return yScale(d[1]);})
-      .attr('r', 5);
+      .attr('r', 2.5);
 var txt = txt.enter().append("text")
       .attr('x', function(d) {return xScale(d[0]);})
       .attr('y', function(d) {return yScale(d[1]);})
@@ -120,9 +86,26 @@ svg.append("rect")
 //   }
 //   return data
 // }
+// svg.call(zoom.transform, transform)
+//     .call(transition);
+// function transform() {
+//   return d3.zoomIdentity
+//       .translate(width / 2, height / 2)
+//       .scale(8)
+//       .translate(data[0][0], data[0][1]);
+// }
+
+// function transition(canvas) {
+//   svg.transition()
+//       .delay(500)
+//       .duration(1000)
+//       .call(zoom.transform, transform);
+// }
 
 function zoomed() {
 // create new scale ojects based on event
+// translate = zoom.translate();
+//   scale = zoom.scale();
     var new_xScale = d3.event.transform.rescaleX(xScale);
     var new_yScale = d3.event.transform.rescaleY(yScale);
 // update axes
@@ -134,4 +117,27 @@ function zoomed() {
      txt.data(data)
      .attr('x', function(d) {return new_xScale(d[0]);})
      .attr('y', function(d) {return new_yScale(d[1]);});
+}
+function myfunction() {
+        var searchvalue=document.getElementById("searchval").value;
+        var p=data.length;
+
+        for(i=0;i<data.length;i++)
+        {
+            if(data[i][2]==searchvalue)
+            {
+                p=i;
+                break;
+            }
+        }
+        if(p<data.length)
+        {
+        point=data[p];
+        document.getElementById("print").innerHTML=p+" "+point[0]+" "+point[1];
+        // return zoom.translate([width / 2 - point[0] * 10, height / 2 - point[1] * 10])
+        //   .scale(10);
+        zoom.scaleTo(d3.select(this), 2)
+        // svg.call(zoom);
+  }
+  // zoom.translateTo(s, x, y)
 }
